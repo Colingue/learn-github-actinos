21e1ab00ff280a60857b5dbe21c2df1e86fc5460
colingue <colin.noiret@gmail.com>
2025-04-30 23:41:06 +0200

    ci test

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     <div>
       <h1>Home</h1>
       <h2>Colin</h2>
-      <Link href="/about">About</Link>
+      <Link href="/about">About coucou je suis la CI</Link>
     </div>
   );
 }
